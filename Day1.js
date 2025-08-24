38ca872a05ba30a420cfac924414e72609c00dba
Priya Sharma <priyaasharmaa2101@gmail.com>
2025-08-24 14:20:43 +0530

    Add files via upload

---
+++ Day1.js
@@ -0,0 +1,12 @@
+const student = {
+  name: "Priya Sharma",
+  age: 19,
+  grade: "A",
+  course: "Computer Science"
+};
+
+console.log("Student Details:");
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("Grade:", student.grade);
+console.log("Course:", student.course);
